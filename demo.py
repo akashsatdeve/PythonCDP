ee54ac675fbc7e4775393747298c87f6b7edc962
akashsatdeve <akashsatdeve8@gmail.com>
2023-10-19 11:30:53 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a=[]
+# b=()
+# c={"key":"value"}
+# dictionary={"name":"raj","age":23}
+# dictionary["DOB"]="19-oct-2000"
+# print(dictionary)
+# print(type(dictionary))
+# print(dictionary.keys())
+# print(dictionary.values())
+
+# for x,y in dictionary.items():
+#     print(x,y)
+    
+    
+# for a in dictionary.keys():
+#     print(a) 
+    
+# for b in dictionary.values():
+#     print(b)          
+    
